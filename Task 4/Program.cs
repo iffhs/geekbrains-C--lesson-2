8feb383b8b4d236564599038bf0aaac5e7a0e1a6
Egor <kosterev2k17@yandex.ru>
2022-10-06 00:18:20 +0300

    first commit

--- Task 4/Program.cs
+++ Task 4/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+System.Console.WriteLine("Введите день недели ");
+int a = Convert.ToInt32 (Console.ReadLine());
+if (a == 6 || a ==7)
+{
+Console.WriteLine("Да");
+}
+if (a > 7)
+{
+Console.WriteLine("Всего 7 дней в неделе");
+}
+else
+{
+Console.WriteLine("Нет");
+}
